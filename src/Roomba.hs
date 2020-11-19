15e41a0a23e8b8bc5e064e20a2126bc2691ca683
Sandy Maguire <sandy@sandymaguire.me>
2020-11-18 17:28:10 -0800

    Improved roomba --- now without a stupid hole in the middle

--- src/Roomba.hs
+++ src/Roomba.hs
@@ -2,79 +2,75 @@ module Roomba where
 
 import Lib
 import StdParts
-import Graphics.Implicit.Primitives (Object(getBox))
-import Data.Maybe (fromJust)
 
 
 main :: IO ()
 main = do
-  writeSTL 1 "/tmp/roomba.stl" $ do
-    fromJust $ pack3 (500, 500) 10 $ split basePlate
+  writeSTL 0.5 "/tmp/roomba.stl" $ do
+    basePlate
     -- union
     --   [ head $ split basePlate
     --   ]
 
-
-
-------------------------------------------------------------------------------
-
-vacuumBagRadius :: R
-vacuumBagRadius = 55
-
-
-additionalRadius :: R
-additionalRadius = 130 - vacuumBagRadius
-
 baseThickness :: R
 baseThickness = 2.5
 
-wheelRadPos :: R
-wheelRadPos = vacuumBagRadius + additionalRadius + yellowWheelRadius / 2
-
-wheelDegPos :: R
-wheelDegPos = 140
-
-halfPos :: R
-halfPos = vacuumBagRadius + additionalRadius /2
+thickness :: R
+thickness = 1.5
 
 
-agitatorDegPos :: R
-agitatorDegPos = 55
+plateSize :: R
+plateSize = 180
 
-------------------------------------------------------------------------------
+
+stockPlate :: SymbolicObj3
+stockPlate
+  = slamTop
+  $ center3
+  $ extrude baseThickness
+  $ union
+  $ let half = plateSize / 2
+     in [ slamBack $ rectR half zero $ mk2 plateSize plateSize
+        , slamBack $ rectR 0 zero $ mk2 plateSize half
+        ]
 
 basePlate :: SymbolicObj3
-basePlate = intersect
-  [ union
-    [ translate (0, 0, -baseThickness) $
-        difference
-          [ cylinder (vacuumBagRadius + additionalRadius) baseThickness
-          , cylinder  vacuumBagRadius baseThickness
-          , symmetrically halfPos agitatorDegPos agitatorHoleBB
-          , symmetrically wheelRadPos wheelDegPos yellowWheelBB
-          ]
-    , translate (mk3 (-3) 58 0) $
-        withPolarPos wheelRadPos (-wheelDegPos) $
-          rotate3 (degZ (-90)) $
-            wedge 15 15 15
-      -- centeredBox (baseThickness * 2) 20 30
-      -- wheel motor
-    , symmetrically (halfPos + 45 / 2) 145 $ extrudedSlot 2 2 $ centeredBox 19 65 10
-    , symmetrically halfPos agitatorDegPos agitatorSlot
-    , withPolarPos halfPos (-80) $ rotate90Z arduinoMiniSlot
-    , withPolarPos halfPos (-105) $ rotate90Z miniBreadboardSlot
-    , withPolarPos halfPos 180 l298nSlot
-    , withPolarPos halfPos 98 $ doubleAAHolderSlot
-    , withPolarPos halfPos 0 $ rotate90Z doubleAAHolderSlot
+basePlate = slamBottom $
+  difference
+    [ union
+        [ stockPlate
+        , translate (mk3 (-wheelXOffset) (wheelYOffset - yellowWheelMotorAxelOffset) 0) $ slamLeft  $ slamFront yellowWheelMotorSlot
+        , translate (mk3 ( wheelXOffset) (wheelYOffset - yellowWheelMotorAxelOffset) 0) $ slamRight $ slamFront yellowWheelMotorSlot
+        , translate (mk3 0 (wheelYOffset + 13) 0) $ slamFront l298nSlot
+        , translate (mk3 0 40 0) $ slamFront ovonicLipoBatterySlot
+        , translate (mk3 (-agitatorXOffset) agitatorYOffset 0) agitatorSlot
+        , translate (mk3 ( agitatorXOffset) agitatorYOffset 0) agitatorSlot
+        , translate (mk3 0 intakeHoleYOffset 0) $ shell baseThickness intakeHoleBB
+        , translate (mk3 0 17.5 0) $ slamBack $ slamBottom containerSlot
+        ]
+    , translate (mk3 (-wheelXOffset) wheelYOffset 0) $ slamRight yellowWheelBB
+    , translate (mk3 ( wheelXOffset) wheelYOffset 0) $ slamLeft yellowWheelBB
+    , translate (mk3 (-agitatorXOffset) agitatorYOffset 0) agitatorHoleBB
+    , translate (mk3 ( agitatorXOffset) agitatorYOffset 0) agitatorHoleBB
+    , translate (mk3 0 intakeHoleYOffset (-baseThickness)) intakeHoleBB
+    , translate (mk3 0 (intakeHoleYOffset - baseThickness) 0) bagHoleBB
     ]
-  , basePlateBounding
-  ]
 
-rotate90Z :: SymbolicObj3 -> SymbolicObj3
-rotate90Z = rotate3 (degZ 90)
+containerYOffset :: R
+containerYOffset = 0
 
-basePlateBounding :: SymbolicObj3
-basePlateBounding = translate (0, 0, -baseThickness) $ cylinder (vacuumBagRadius + additionalRadius) 100
+wheelXOffset :: R
+wheelXOffset = 50
+
+wheelYOffset :: R
+wheelYOffset = -85
+
+agitatorXOffset :: R
+agitatorXOffset = 60
+
+agitatorYOffset :: R
+agitatorYOffset = 50
+
 
 agitatorHoleBB :: SymbolicObj3
 agitatorHoleBB = centeredBox 15 15 25
@@ -82,5 +78,32 @@ agitatorHoleBB = centeredBox 15 15 25
 agitatorSlot :: SymbolicObj3
 agitatorSlot = extrudedSlot 2 2 $ centeredBox 21 21 25
 
+containerBB :: SymbolicObj3
+containerBB = centeredBox 118 33 63
 
+container :: SymbolicObj3
+container = shell baseThickness containerBB
+
+containerSlot :: SymbolicObj3
+containerSlot
+  = extrudedSlot thickness 10
+  $ expand (mk3 baseThickness baseThickness baseThickness)
+  $ containerBB
+
+
+
+intakeHoleBB :: SymbolicObj3
+intakeHoleBB = slamBack $ slamBottom $ centeredBox 130 15 intakeHoleZSize
+
+intakeHoleYOffset :: R
+intakeHoleYOffset = 10 + containerYOffset + 15 + 7
+
+bagHoleXSize :: R
+bagHoleXSize = 30
+
+intakeHoleZSize :: R
+intakeHoleZSize = 20
+
+bagHoleBB :: SymbolicObj3
+bagHoleBB = slamBack $ slamBottom $ centeredBox bagHoleXSize 20 (intakeHoleZSize / 2)
 
--- src/StdParts.hs
+++ src/StdParts.hs
@@ -1,7 +1,6 @@
 module StdParts where
 
 import Lib
-import Graphics.Implicit
 
 
 yellowWheelBB :: SymbolicObj3
@@ -16,6 +15,7 @@ yellowWheelBB =
     clearance = 4
     total_clearance = clearance * 2
 
+
 yellowWheelRadius :: R
 yellowWheelRadius = 68 / 2
 
@@ -23,21 +23,38 @@ yellowWheelRadius = 68 / 2
 yellowWheelMotorWidth :: R
 yellowWheelMotorWidth = 19
 
+-- TODO(sandy): measure me
+yellowWheelMotorAxelOffset :: R
+yellowWheelMotorAxelOffset = 8
+
+yellowWheelMotorSlot :: SymbolicObj3
+yellowWheelMotorSlot = extrudedSlot 2 2 $ centeredBox 19 65 10
+
+
 tabXPosFacing :: SymbolicObj3
 tabXPosFacing = box 2 5 7
+
 
 tabXNegFacing :: SymbolicObj3
 tabXNegFacing = rotate3 (degZ pi) $ box 2 5 7
 
+
 arduinoMiniSlot :: SymbolicObj3
 arduinoMiniSlot = extrudedSlot 2 2 $ centeredBox 18 45 10
+
 
 l298nSlot :: SymbolicObj3
 l298nSlot = extrudedSlot 2 2 $ centeredBox 44 44 10
 
+
 miniBreadboardSlot :: SymbolicObj3
 miniBreadboardSlot = extrudedSlot 2 2 $ centeredBox 35 47 10
 
+
 doubleAAHolderSlot :: SymbolicObj3
 doubleAAHolderSlot = extrudedSlot 2 2 $ centeredBox 32.4 85.5 10
+
+
+ovonicLipoBatterySlot :: SymbolicObj3
+ovonicLipoBatterySlot = extrudedSlot 2 2 $ centeredBox 69 32 10
 
